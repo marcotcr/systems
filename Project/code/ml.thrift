64422d8be3a5a70b926914ada32d45e386827dec
Marco Tulio Ribeiro <marcotcr@gmail.com>
2014-03-08 10:41:50 -0800

    New thrift interface.

--- Project/code/ml.thrift
+++
@@ -1,8 +0,0 @@
-service MLEngine {
-  double Predict(1:i32 id, 2:string x),
-  void Learn(1:string x, 2:double y),
-  /* Learn from a previously predicted example that has been kept in memory */
-  void LearnFromID(1:i32 id, 2:double y),
-  double AvgPredictionTime(),
-  oneway void Test()
-}
