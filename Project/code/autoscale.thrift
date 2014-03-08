34b733d4c4325f6fe70632d9b00009e3fdaa886d
Marco Tulio Ribeiro <marcotcr@gmail.com>
2014-03-08 11:05:37 -0800

    autoscale.

---
+++ Project/code/autoscale.thrift
@@ -0,0 +1,15 @@
+service LoadBalancer {
+  string GetNode(),
+  void SetNodes(1:map<string, string> state)
+}
+service Worker {
+  double Predict(1:i32 id, 2:string x),
+  void Learn(1:string x, 2:double y),
+  /* Learn from a previously predicted example that has been kept in memory */
+  void LearnFromID(1:i32 id, 2:double y),
+  double AvgPredictionTime(),
+  oneway void Test()
+}
+service AutoScaler {
+  void HeartBeat(1:map<string, string> state)
+}
