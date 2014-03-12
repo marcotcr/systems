96136f9614ec4804202995d366941e757c5bff47
Shrainik Jain <shrainik@gmail.com>
2014-03-12 01:36:16 -0700

    Scripts to run all experiments

---
+++ Project/code/gen_graphs.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+python graphs.py -n ../Logs/Naive1.8.2/nodes -t ../Logs/Naive1.8.2/pred_time -r ../Logs/Naive1.8.2/requests -p 1
+python graphs.py -n ../Logs/Power1.8.2/nodes -t ../Logs/Power1.8.2/pred_time -r ../Logs/Power1.8.2/requests -p 1
+python graphs.py -n ../Logs/Smart1.8.2/nodes -t ../Logs/Smart1.8.2/pred_time -r ../Logs/Smart1.8.2/requests -p 1
+
+python graphs.py -n ../Logs/Naive2.8.2/nodes -t ../Logs/Naive2.8.2/pred_time -r ../Logs/Naive2.8.2/requests -p 2
+python graphs.py -n ../Logs/Power2.8.2/nodes -t ../Logs/Power2.8.2/pred_time -r ../Logs/Power2.8.2/requests -p 2
+python graphs.py -n ../Logs/Smart2.8.2/nodes -t ../Logs/Smart2.8.2/pred_time -r ../Logs/Smart2.8.2/requests -p 2
+
+python graphs.py -n ../Logs/Naive3.8.2/nodes -t ../Logs/Naive3.8.2/pred_time -r ../Logs/Naive3.8.2/requests -p 3
+python graphs.py -n ../Logs/Power3.8.2/nodes -t ../Logs/Power3.8.2/pred_time -r ../Logs/Power3.8.2/requests -p 3
+python graphs.py -n ../Logs/Smart3.8.2/nodes -t ../Logs/Smart3.8.2/pred_time -r ../Logs/Smart3.8.2/requests -p 3
+
+
+python graphs.py -n ../Logs/Naive1.8.8/nodes -t ../Logs/Naive1.8.8/pred_time -r ../Logs/Naive1.8.8/requests -p 1
+python graphs.py -n ../Logs/Power1.8.8/nodes -t ../Logs/Power1.8.8/pred_time -r ../Logs/Power1.8.8/requests -p 1
+python graphs.py -n ../Logs/Smart1.8.8/nodes -t ../Logs/Smart1.8.8/pred_time -r ../Logs/Smart1.8.8/requests -p 1
+
+python graphs.py -n ../Logs/Naive2.8.8/nodes -t ../Logs/Naive2.8.8/pred_time -r ../Logs/Naive2.8.8/requests -p 2
+python graphs.py -n ../Logs/Power2.8.8/nodes -t ../Logs/Power2.8.8/pred_time -r ../Logs/Power2.8.8/requests -p 2
+python graphs.py -n ../Logs/Smart2.8.8/nodes -t ../Logs/Smart2.8.8/pred_time -r ../Logs/Smart2.8.8/requests -p 2
+
+python graphs.py -n ../Logs/Naive3.8.8/nodes -t ../Logs/Naive3.8.8/pred_time -r ../Logs/Naive3.8.8/requests -p 3
+python graphs.py -n ../Logs/Power3.8.8/nodes -t ../Logs/Power3.8.8/pred_time -r ../Logs/Power3.8.8/requests -p 3
+python graphs.py -n ../Logs/Smart3.8.8/nodes -t ../Logs/Smart3.8.8/pred_time -r ../Logs/Smart3.8.8/requests -p 3
+
+python graphs.py -n ../Logs/Smart2.97.2/nodes -t ../Logs/Smart2.97.2/pred_time -r ../Logs/Smart2.97.2/requests -p 2
+python graphs.py -n ../Logs/Smart1.97.2/nodes -t ../Logs/Smart1.97.2/pred_time -r ../Logs/Smart1.97.2/requests -p 1
